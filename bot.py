65f8491c255803e3ec7b0b5472210acdd638dbf8
xViperAG <usairsoftingandgaming@gmail.com>
2024-10-27 18:40:31 -0400

    Update

--- bot.py
+++ bot.py
@@ -1,37 +1,79 @@
 import discord
+from discord.ext import commands
 import json
 
-client = discord.Client()
-@client.event
-async def on_ready():
-    print("Connected to Discord")
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
 
 @client.event
 async def on_message(msg):
-    print(msg.author," sent ",msg.content)
+    print(msg.author," sent ", msg.content)
     if msg.content.startswith("!"):
         cmd=msg.content.replace("!","",1)
         cmdname=cmd.split(" ")[0]
-        print(cmdname)
-        if cmdname=="intro":
-            embed = discord.Embed(title="Welcome",description="**Hello "+msg.author.mention+"! Welcome to Alpha Base. This is the home of USAG Viper. Check out all the channels. One rule. DON'T BE A DICK.**", colour=discord.Colour.teal());
+
+        if cmdname=="welcome":
+            embed = discord.Embed(
+                title="Welcome",
+                description="**Hello " + msg.author.mention + "!**  \n  \nWelcome to Rising Hope. I am your Phoenix API. We are currently under development, but let\'s get you started.",
+                colour=discord.Colour.dark_red()
+            )
+            embed.set_author(name='Phoenix Specialists Division', icon_url="https://i.postimg.cc/J79qCRsV/Rising-Hope-RP-1.png")
+            embed.set_thumbnail(url='https://i.postimg.cc/J79qCRsV/Rising-Hope-RP-1.png')
+            embed.set_image(url='https://i.postimg.cc/zGzRjGdt/Untitled_design_(2).png')
             await msg.channel.send(embed=embed)
-        print(cmdname)
+
         if cmdname=="website":
-            embed = discord.Embed(title="Website",description="**Error 404: Website Under Construction. Check with us soon.**", colour=discord.Colour.teal());
+            embed = discord.Embed(
+                title="Website",
+                description="If you are wanting to support us on our journey, don't be afraid to join us at the following links!  \n  \n**LINKS:**  \n[Documentation](https://rising-hope-rp.gitbook.io/rising-hope-rp-docs/)  \n[Tebex](https://risinghoperp.tebex.io/)",
+                colour=discord.Colour.dark_red(),
+            )
+            embed.set_author(name='Phoenix Specialists Division', icon_url="https://i.postimg.cc/J79qCRsV/Rising-Hope-RP-1.png")
+            embed.set_thumbnail(url='https://i.postimg.cc/J79qCRsV/Rising-Hope-RP-1.png')
+            embed.set_image(url='https://i.postimg.cc/zGzRjGdt/Untitled_design_(2).png')
             await msg.channel.send(embed=embed)
-        print(cmdname)
-        if cmdname=="live":
-            embed = discord.Embed(title="Live",description="**Hey "+msg.guild.default_role.mention+", I'm live on Twitch right now! https://www.twitch.tv/us_airsofting_and_gaming**", colour=discord.Colour.teal());
-            await msg.channel.send(embed=embed)
-        print(cmdname)
-        if cmdname=="channel":
-            embed = discord.Embed(title="Channels",description="**Hey "+msg.author.mention+", I'm on YouTube and Twitch! Youtube:https://www.youtube.com/channel/UCyH_IzCfV_Uh0_rD6w8UKYA Twitch: https://twitch.tv/us_airsofting_and_gaming**", colour=discord.Colour.teal());
-            await msg.channel.send(embed=embed)
-        print(cmdname)
+
         if cmdname=="commands":
-            embed = discord.Embed(title="Commands",description="**The current commands that we have are !welcome, !website, and !channels at the moment with more to be added.**", colour=discord.Colour.teal());
+            embed = discord.Embed(
+                title="Commands",
+                description="**The current commands that we have are !welcome, !website, and !channels at the moment with more to be added.**",
+                colour=discord.Colour.dark_red()
+            )
+            embed.set_author(name='Phoenix Specialists Division', icon_url="https://i.postimg.cc/J79qCRsV/Rising-Hope-RP-1.png")
+            embed.set_thumbnail(url='https://i.postimg.cc/J79qCRsV/Rising-Hope-RP-1.png')
+            embed.set_image(url='https://i.postimg.cc/zGzRjGdt/Untitled_design_(2).png')
             await msg.channel.send(embed=embed)
+
+        if cmdname=="verifyme":
+            embed = discord.Embed(
+                title = "Welcome to Rising Hope RP!",
+                description = "After you have read the rules in the Server Docs, make sure to react to this message to allow for you to gain the roles for reading the rules!  \n  \nBe sure to follow the rules of the Discord as you are required to follow them at all times!",
+                colour = discord.Colour.dark_red()
+            )
+            embed.set_author(name='Phoenix Specialists Division', icon_url="https://i.postimg.cc/J79qCRsV/Rising-Hope-RP-1.png")
+            embed.set_thumbnail(url='https://i.postimg.cc/J79qCRsV/Rising-Hope-RP-1.png')
+            embed.set_image(url='https://i.postimg.cc/zGzRjGdt/Untitled_design_(2).png')
+            await msg.channel.send(embed=embed)
+
+        if cmdname=="changelog":
+            if msg.author.roles == { '@Developer' }:
+                input = discord.ui.TextInput(
+
+                )
+
+                await input.callback()
+                embed = discord.Embed(
+                    title = 'New Changelog',
+                    description = 'What\'s new:'
+                )
+                await msg.channel.send(embed.embed)
 
 @client.event
 async def on_ready():
@@ -39,7 +81,15 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    embed = discord.Embed(title="Hello",description="Welcome to Rising Hope. I am your Phoenix API. We are currently under development, but let\'s get you started.", colour=discord.Colour.teal());
+    embed = discord.Embed(
+        title="Hello",
+        description="Welcome to Rising Hope. I am your Phoenix API. We are currently under development, but let\'s get you started.",
+        colour=discord.Colour.dark_red()
+    )
+    embed.set_author(name='Phoenix Specialists Division', icon_url="https://i.postimg.cc/J79qCRsV/Rising-Hope-RP-1.png")
+    embed.set_thumbnail(url='https://i.postimg.cc/J79qCRsV/Rising-Hope-RP-1.png')
+    embed.set_image(url='https://i.postimg.cc/zGzRjGdt/Untitled_design_(2).png')
+
     try:
         await member.send(embed=embed)
     except: pass
